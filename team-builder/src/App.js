4ea8c8d185b966a0b634d75f280f8dee674a2350
Erik <erik.solveson@gmail.com>
2021-06-06 19:03:33 -0600

    created simple add team member form

---
+++ team-builder/src/App.js
@@ -0,0 +1,59 @@
+import "./App.css";
+import React, { useState } from "react";
+const initialFormValues = { memberName: "", memberRole: "", memberEmail: "" };
+function App() {
+  const [formValue, setFormValue] = useState(initialFormValues);
+  const [memberList, setMemberList] = useState([]);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newMember = formValue;
+    const updatedMemberList = memberList.concat(newMember);
+    setMemberList(updatedMemberList);
+  };
+
+  const changeHander = (event) => {
+    const inputName = event.target.name;
+    const inputValue = event.target.value;
+    setFormValue({ ...formValue, [inputName]: inputValue });
+  };
+
+  return (
+    <div className="App">
+      <h1>Team Members Form</h1>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          name="memberName"
+          value={formValue.memberName}
+          onChange={changeHander}
+        />
+        <br />
+        <input
+          type="text"
+          name="memberEmail"
+          value={formValue.memberEmail}
+          onChange={changeHander}
+        />
+        <br />
+        <input
+          type="text"
+          name="memberRole"
+          value={formValue.memberRole}
+          onChange={changeHander}
+        />
+        <br />
+        <button>Submit</button>
+      </form>
+      {memberList.map((element) => (
+        <div class="member">
+          <p>Name: {element.memberName}</p>
+          <p>Role: {element.memberRole}</p>
+          <p>Email: {element.memberEmail}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
